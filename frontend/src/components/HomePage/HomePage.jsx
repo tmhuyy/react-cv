f8e302515fc2fb11d03cad2a8e9d78907fa5330b
Huy Tran <tmhuyy.itiu@gmail.com>
2022-04-29 03:38:03 +0700

    create home page

---
+++ frontend/src/components/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import ContentLP from "../ContentLP/ContentLP";
+import Header from "../Header/Header";
+
+const HomePage = () => {
+    return ( 
+        <>
+            <Header />
+            <ContentLP/>
+        </>
+     );
+}
+ 
+export default HomePage;
