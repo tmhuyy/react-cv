cf6cfaa369e3eae95f4cf7b4a2da340772cd6cd7
Huy Tran <tmhuyy.itiu@gmail.com>
2022-04-28 10:48:48 +0700

    finish demo model with Author and Book

---
+++ backend/model/model.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+// authorSchema
+const authorSchema = new mongoose.Schema({
+    name: {
+        type: String
+    },
+    year: {
+        type: Number
+    },
+    books: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Book"
+        }
+    ]
+})
+// bookSchema
+const bookSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    publishDate: {
+        type: String
+    },
+    genres: {
+        type: [String]
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "Author"
+    }
+});
+const Book = mongoose.model("Book", bookSchema);
+const Author = mongoose.model("Author", authorSchema)
+module.exports = { Book, Author };
