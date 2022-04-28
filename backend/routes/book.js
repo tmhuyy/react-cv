1b0d1c787bc16465639d5a8cff945a2dd214843f
Huy Tran <tmhuyy.itiu@gmail.com>
2022-04-28 13:31:38 +0700

    create GET ALL BOOK request

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -4,5 +4,6 @@ const router = require("express").Router();
 
 // ADD BOOK
 router.post("/", bookController.addBook)
-
+// GET ALL BOOKS
+router.get("/", bookController.getAllBook)
 module.exports = router;
