712bc6bbc57e1d96d969134ac180cbbba4f0ba43
Huy Tran <tmhuyy.itiu@gmail.com>
2022-04-28 23:55:46 +0700

    create ADD EDUCATION request

--- backend/controllers/educationController.js
+++ backend/controllers/educationController.js
@@ -1,5 +1,16 @@
-const educationController = {
+const { Link, Education, Project, Skill, User } = require("../model/model");
 
+const educationController = {
+    // ADD EDUCATION
+    addEducation: async (req, res) => {
+        try {
+            const newEducation = new Education(req.body);
+            const savedEducation = await newEducation.save();
+            res.status(200).json(savedEducation);
+        } catch(err) {
+            res.status(500).json(err); 
+        }
+    }
 };
 
 module.exports = educationController
--- backend/routes/education.js
+++ backend/routes/education.js
@@ -3,6 +3,6 @@ const educationController = require("../controllers/educationController");
 const router = require("express").Router();
 
 // ADD USER
-router.post("/", educationController);
+router.post("/", educationController.addEducation);
 
 module.exports = router;
