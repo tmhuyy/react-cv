14d4546a524c93be96df7f217c05d78d4db4d34e
Huy Tran <tmhuyy.itiu@gmail.com>
2022-04-29 00:26:16 +0700

    create GET ALL USERS request

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -10,6 +10,15 @@ const userController = {
         } catch (err) {
             res.status(500).json(err)
         }
+    },
+    // GET ALL USERS
+    getAllUsers: async (req, res) => {
+        try {
+            const users = await User.find();
+            res.status(200).json(users);
+        } catch (err) {
+            res.status(500).json(err);
+        }
     }
 }
 module.exports = userController;
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,4 +5,7 @@ const router = require("express").Router();
 // ADD USER
 router.post("/", userController.addUser);
 
+// GET ALL USERS
+router.get("/", userController.getAllUsers);
+
 module.exports = router;
