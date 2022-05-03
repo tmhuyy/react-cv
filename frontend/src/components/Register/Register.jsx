2602b121cf22e11e0059546142d9da48bee8decc
Huy Tran <tmhuyy.itiu@gmail.com>
2022-05-04 01:26:57 +0700

    config responsive rendering web page for user

--- frontend/src/components/Register/Register.jsx
+++ frontend/src/components/Register/Register.jsx
@@ -8,65 +8,65 @@ const Register = (props) => {
         <>
             {data && 
             <section className="mx-4 mt-4">
-            <div className="text-center mb-4">
-                <h1 className="">{`${data.firstName} ${data.lastName}`}</h1>
-                <p className="text-[10px]">{data.address}</p>
-                <p className="text-[10px]">{data.phoneNumber}</p>
-                {data.links.map((link, index) => {
-                    return (
-                        <a key={index} href={link.url} className="text-[10px] mr-4">{ link.name }</a>
-                    )
-                })}
-            </div>
-            <div className="mb-4">
-                <h2 className="">CAREER OBJECTIVE</h2>
-                <hr className="border-black dark:border-white"></hr>
-                <p className="text-[10px]">{ data.careerObjective}</p>
-            </div>
-            <div className="mb-4">
-                <h2>EDUCATION</h2>
-                <hr className="border-black dark:border-white"></hr>
-                {data.educations.map((education, index) => {
-                    return (
-                        <section key={index}>
-                            <div className="flex flex-row justify-between mb-2">
-                                <p className="text-[11px] font-bold">{ education.name}</p>
-                                <p className="text-[11px] font-bold">{`${moment(education.startDate).format("MMM YYYY")} - ${moment(education.endDate).format("MMM YYYY")}`}</p>
-                            </div>
-                            <p className="text-[11px]">{education.degree}</p>
-                            <p className="text-[11px]">{education.details}</p>
-                        </section>
-                    )
-                })}
-                
-            </div>
-            <div className="mb-4">
-                <h2>SKILLS</h2>
-                <hr className="border-black dark:border-white"></hr>
-                <ul className="grid grid-cols-2">
-                    {data.skills.map((skill, index) => {
+                <div className="text-center mb-4">
+                    <h1 className="md:text-[25px]">{`${data.firstName} ${data.lastName}`}</h1>
+                    <p className="text-[10px] md:text-[15px]">{data.address}</p>
+                    <p className="text-[10px] md:text-[15px]">{data.phoneNumber}</p>
+                    {data.links.map((link, index) => {
                         return (
-                            <li key={index} className="text-[11px] list-disc">{`${skill.name}: ${skill.details}`}</li>
+                            <a key={index} href={link.url} className="text-[10px] md:text-[15px] mr-4 underline decoration-1">{ link.name }</a>
                         )
                     })}
-                </ul>
-            </div>
-            <div className="mb-4">
-                <h2>PROJECTS</h2>
-                <hr className="border-black dark:border-white"></hr>
-                {data.projects.map((project, index) => {
-                    return (
-                        <section key={index}>
-                            <div className="flex flex-row justify-between">
-                                <p className="text-[11px] font-bold">{ project.name}</p>
-                                <p className="text-[11px] font-bold">{`${moment(project.startDate).format("MMM YYYY")} - ${moment(project.endDate).format("MMM YYYY")}`}</p>
-                            </div>
-                            <p className="text-[11px] mb-4">{ project.details }</p>
-                        </section>
-                    )
-                })}      
-            </div> 
-        </section>}
+                </div>
+                <div className="mb-8">
+                    <h2 className="md:text-[20px]">CAREER OBJECTIVE</h2>
+                    <hr className="md:border-[1px] border-black dark:border-white"></hr>
+                    <p className="text-[10px] md:text-[15px]">{ data.careerObjective}</p>
+                </div>
+                <div className="mb-8">
+                    <h2 className="md:text-[20px]">EDUCATION</h2>
+                    <hr className="md:border-[1px] border-black dark:border-white"></hr>
+                    {data.educations.map((education, index) => {
+                        return (
+                            <section key={index}>
+                                <div className="flex flex-row justify-between mb-2">
+                                    <p className="text-[11px] font-bold md:text-[15px]">{ education.name}</p>
+                                    <p className="text-[11px] font-bold md:text-[15px]">{`${moment(education.startDate).format("MMM YYYY")} - ${moment(education.endDate).format("MMM YYYY")}`}</p>
+                                </div>
+                                <p className="text-[11px] md:text-[15px]">{education.degree}</p>
+                                <p className="text-[11px] md:text-[15px]">{education.details}</p>
+                            </section>
+                        )
+                    })}
+                    
+                </div>
+                <div className="mb-8">
+                    <h2 className="md:text-[20px]">SKILLS</h2>
+                    <hr className="md:border-[1px] border-black dark:border-white"></hr>
+                    <ul className="grid grid-cols-2">
+                        {data.skills.map((skill, index) => {
+                            return (
+                                <li key={index} className="text-[11px] list-disc md:text-[15px]">{`${skill.name}: ${skill.details}`}</li>
+                            )
+                        })}
+                    </ul>
+                </div>
+                <div className="mb-8">
+                    <h2 className="md:text-[20px]">PROJECTS</h2>
+                    <hr className="md:border-[1px] border-black dark:border-white"></hr>
+                    {data.projects.map((project, index) => {
+                        return (
+                            <section key={index}>
+                                <div className="flex flex-row justify-between">
+                                    <p className="text-[11px] font-bold md:text-[15px]">{ project.name}</p>
+                                    <p className="text-[11px] font-bold md:text-[15px]">{`${moment(project.startDate).format("MMM YYYY")} - ${moment(project.endDate).format("MMM YYYY")}`}</p>
+                                </div>
+                                <p className="text-[11px] mb-4 md:text-[15px]">{ project.details }</p>
+                            </section>
+                        )
+                    })}      
+                </div> 
+            </section>}
             
         </>
         
