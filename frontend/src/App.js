6958080a2107db88abda48a0e989a99b2f88488c
Huy Tran <tmhuyy.itiu@gmail.com>
2022-05-07 00:00:51 +0700

    delete all css files and use destructuring operator

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,4 @@
-// import './App.css';
 import { useEffect, useState } from "react";
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
 import HomePage from "./components/HomePage/HomePage";
--- frontend/src/components/Register/Register.jsx
+++ frontend/src/components/Register/Register.jsx
@@ -1,18 +1,16 @@
 import moment from "moment"
 const Register = (props) => {
     const { allUsers } = props;
-    const data = allUsers[0];
-    console.log(data)
+    const [newestUser] = allUsers;
     return ( 
-        
         <>
-            {data && 
+            {newestUser && 
             <section className="mx-4 mt-4">
                 <div className="text-center mb-4">
-                    <h1 className="md:text-[25px]">{`${data.firstName} ${data.lastName}`}</h1>
-                    <p className="text-[10px] md:text-[15px]">{data.address}</p>
-                    <p className="text-[10px] md:text-[15px]">{data.phoneNumber}</p>
-                    {data.links.map((link, index) => {
+                    <h1 className="md:text-[25px]">{`${newestUser.firstName} ${newestUser.lastName}`}</h1>
+                    <p className="text-[10px] md:text-[15px]">{newestUser.address}</p>
+                    <p className="text-[10px] md:text-[15px]">{newestUser.phoneNumber}</p>
+                    {newestUser.links.map((link, index) => {
                         return (
                             <a key={index} href={link.url} className="text-[10px] md:text-[15px] mr-4 underline decoration-1">{ link.name }</a>
                         )
@@ -21,12 +19,12 @@ const Register = (props) => {
                 <div className="mb-8">
                     <h2 className="md:text-[20px]">CAREER OBJECTIVE</h2>
                     <hr className="md:border-[1px] border-black dark:border-white"></hr>
-                    <p className="text-[10px] md:text-[15px]">{ data.careerObjective}</p>
+                    <p className="text-[10px] md:text-[15px]">{ newestUser.careerObjective}</p>
                 </div>
                 <div className="mb-8">
                     <h2 className="md:text-[20px]">EDUCATION</h2>
                     <hr className="md:border-[1px] border-black dark:border-white"></hr>
-                    {data.educations.map((education, index) => {
+                    {newestUser.educations.map((education, index) => {
                         return (
                             <section key={index}>
                                 <div className="flex flex-row justify-between mb-2">
@@ -44,7 +42,7 @@ const Register = (props) => {
                     <h2 className="md:text-[20px]">SKILLS</h2>
                     <hr className="md:border-[1px] border-black dark:border-white"></hr>
                     <ul className="grid grid-cols-2">
-                        {data.skills.map((skill, index) => {
+                        {newestUser.skills.map((skill, index) => {
                             return (
                                 <li key={index} className="text-[11px] list-disc md:text-[15px]">{`${skill.name}: ${skill.details}`}</li>
                             )
@@ -54,7 +52,7 @@ const Register = (props) => {
                 <div className="mb-8">
                     <h2 className="md:text-[20px]">PROJECTS</h2>
                     <hr className="md:border-[1px] border-black dark:border-white"></hr>
-                    {data.projects.map((project, index) => {
+                    {newestUser.projects.map((project, index) => {
                         return (
                             <section key={index}>
                                 <div className="flex flex-row justify-between">
@@ -67,7 +65,6 @@ const Register = (props) => {
                     })}      
                 </div> 
             </section>}
-            
         </>
         
      );
